4003c4eb98327c6babc0085e4e205e4a45259168
lichgo <lichgo88@gmail.com>
2014-05-05 07:41:17 +0800

    logic helper

--- projects/assets/LogicHelper.scala
+++ projects/assets/LogicHelper.scala
@@ -4,16 +4,39 @@ import scala.actors._
 import Actor._
 
 object LogicHelper {
-  val stockUnitMap = StockPriceSpider.getStockUnitsMap
+  val stockUnitsMap = StockPriceSpider.getStockUnitsMap
   
   def genInitTableData: Array[Array[Any]] = {
     val datasource = new Array[Array[Any]](0)
-    (datasource /: stockUnitMap) {
+    (datasource /: stockUnitsMap) {
       (data, element) =>
         val (symbol, units) = element
         data ++ Array(List(symbol, units, "?", "?").toArray)
     }
   }
   
-  // intermediate actor bridging [UI thread] and [fetching threads]
+  // intermediate actor bridging [UI thread:updater] and [fetching threads]
+  def fetchPrice(updater: Actor) = actor {
+    val caller = self
+    
+    stockUnitsMap.keys.foreach {
+      symbol => actor { caller ! (symbol, StockPriceSpider.getLatestClosingPrice(symbol)) }
+    }
+    
+    // receive stockUnitsMap.size times
+    val netWorth = (0.0 /: (1 to stockUnitsMap.size)) {
+      (worth, index) => {
+        receiveWithin(10000) {
+          case (symbol: String, latestClosingPrice: Double) =>
+            val units = stockUnitsMap(symbol)
+            val value = units * latestClosingPrice
+            updater ! (symbol, units, latestClosingPrice, value)
+            worth + value
+        }
+      }
+    }
+    
+    updater ! netWorth
+  }
+  
 }
